04628c4089f24d3599c58c158ea877d200667e64
Brian Gerkey <gerkey@osrfoundation.org>
2014-02-27 19:35:07 -0800

    add lion bottle

--- opensplice.rb
+++ opensplice.rb
@@ -21,6 +21,7 @@ class Opensplice < Formula
     revision 1
     sha1 "84aab9ca20dccb786f4d9cc0030547e7b25f67c1" => :mavericks
     sha1 "99bdb8890854eec049bf7ad0dc358777eca93d1c" => :mountain_lion
+    sha1 "b86bb548334f632783cc9b0107cd48888eaf9db2" => :lion
   end
 
   head 'https://github.com/osrf/opensplice', :using => :git
